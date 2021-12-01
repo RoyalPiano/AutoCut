6c608b643b2d198e7caf212ad37c2c7eeda95045
Mckupe <65507746+Mckupe@users.noreply.github.com>
2021-12-01 17:32:28 +0500

    AddLock
    Доделал метод по изменению размера и добавил метод по добавлению замков

--- ProjectZavod/ViewModels/MainWindowVM.cs
+++ ProjectZavod/ViewModels/MainWindowVM.cs
@@ -1,6 +1,9 @@
 ﻿using netDxf;
+using netDxf.Entities;
 using netDxf.Header;
+using netDxf.Objects;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
@@ -234,18 +237,25 @@ namespace ProjectZavod.ViewModels
             }
         }
 
-        private void ChangeSize(DxfDocument ourFile, double width, double height)
+        private DxfDocument ChangeSize(DxfDocument ourFile, double width, double height)
         {
             double newWidth = width - 860;
             double newHeigh = height - 2050; // переменная не используется? 
             foreach (var x in ourFile.Lines)
             {
-                if (x.Color.B == 255)
+                if (x.Color.G == 255)
                     x.EndPoint = x.EndPoint + new Vector3(newWidth, 0, 0);
-                if (x.Color.B == 155)
+                if (x.Color.B == 255)
                 {
                     x.StartPoint = x.StartPoint + new Vector3(newWidth, 0, 0);
                     x.EndPoint = x.EndPoint + new Vector3(newWidth, 0, 0);
+                }
+                if (x.Color.R == 255)
+                    x.EndPoint = x.EndPoint - new Vector3(0, newHeigh, 0);
+                if (x.Color.R == 254)
+                {
+                    x.StartPoint = x.StartPoint - new Vector3(0, newHeigh, 0);
+                    x.EndPoint = x.EndPoint - new Vector3(0, newHeigh, 0);
                 }
             }
             foreach (var x in ourFile.Circles)
@@ -253,6 +263,55 @@ namespace ProjectZavod.ViewModels
                 if (x.Color.B == 255)
                     x.Center = x.Center + new Vector3(newWidth, 0, 0);
             }
+            return ourFile;
+        }
+
+        private static DxfDocument AddLock(DxfDocument ourFile, DxfDocument lockFile, DxfDocument lockFile2)
+        {
+            var listEntites = new List<EntityObject>();
+            if (lockFile != null)
+            foreach (Layout layout in lockFile.Layouts)
+            {
+                List<DxfObject> entities = lockFile.Layouts.GetReferences(layout);
+                foreach (DxfObject o in entities)
+                {
+                    EntityObject entity = o as EntityObject;
+                    listEntites.Add(entity);
+                }
+            }
+            if (lockFile2 != null)
+            foreach (Layout layout in lockFile2.Layouts)
+            {
+                List<DxfObject> entities = lockFile2.Layouts.GetReferences(layout);
+                Vector3 lenghtToSecondHole = new Vector3(ourFile.Points.ToArray()[0].Position.X, 0, 0) - new Vector3(ourFile.Points.ToArray()[1].Position.X, 0, 0);
+                foreach (DxfObject o in entities)
+                {
+                    EntityObject entity = o as EntityObject;
+                    switch ((int)entity.Type)
+                    {
+                        case 1:
+                            var cir = (Circle)entity;
+                            cir.Center = cir.Center + lenghtToSecondHole;
+                            break;
+                        case 10:
+                            var line = (Line)entity;
+                            line.StartPoint = line.StartPoint + lenghtToSecondHole;
+                            line.EndPoint = line.EndPoint + lenghtToSecondHole;
+                            break;
+                        case 0:
+                            var arc = (Arc)entity;
+                            arc.Center = arc.Center + lenghtToSecondHole;
+                            break;
+                    }
+                    listEntites.Add(entity);
+                }
+            }
+            foreach (var y in listEntites)
+            {
+                var x = y.Clone();
+                ourFile.AddEntity((EntityObject)x);
+            }
+            return ourFile;
         }
 
         public void BrowseFolder(ref string path)
